5d2fb42dadf56ff64e0e806e9fc11b9766102250
Giada Vecchi <giada.vecchi@studenti.unicam.it>
2026-06-19 15:47:39 +0200

    implementazione azioni del giocatore

--- src/main/java/it/unicam/cs/mpgc/rpg126362/controller/BattleController.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126362/controller/BattleController.java
@@ -1,5 +1,32 @@
 package it.unicam.cs.mpgc.rpg126362.controller;
 
-public class BattleController {
-    
+import it.unicam.cs.mpgc.rpg126362.model.Battle;
+/**
+ * Manages player actions during combat.
+ */
+public class BattleController implements IBattleController {
+
+    private final Battle battle;
+
+    public BattleController(Battle battle) {
+        this.battle = battle;
+    }
+
+    @Override
+    public Battle getBattle() { return battle; }
+
+    @Override
+    public boolean isFinished() { return battle.isFinished(); }
+
+    @Override
+    public String attack() { return battle.playerAttack(); }
+
+    @Override
+    public String heal() { return battle.playerHeal(); }
+
+    @Override
+    public String buff() { return battle.playerBuff(); }
+
+    @Override
+    public String switchTo(int index) { return battle.playerSwitch(index); }
 }

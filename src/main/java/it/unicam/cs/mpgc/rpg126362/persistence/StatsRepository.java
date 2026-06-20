73ef00f9b044dbd9c969a8bb5db7e5f165f3108b
Giada Vecchi <giada.vecchi@studenti.unicam.it>
2026-06-20 10:25:19 +0200

    aggiunta interfaccia per statistiche

---
+++ src/main/java/it/unicam/cs/mpgc/rpg126362/persistence/StatsRepository.java
@@ -0,0 +1,21 @@
+package it.unicam.cs.mpgc.rpg126362.persistence;
+
+/**
+* Contract for saving and loading global statistics.
+*/
+public interface StatsRepository {
+
+/** Loads statistics. If they don't exist, returns an object with a zero value. */
+Stats load();
+
+/** Saves statistics, updating the data from the last game. */
+void save(Stats stats);
+
+/** DTO with global statistics data. */
+  class Stats {
+        public int wins = 0;
+        public int losses = 0;
+        public String lastDate = "-";
+    }   
+
+}

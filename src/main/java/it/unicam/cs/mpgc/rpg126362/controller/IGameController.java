8f1168be505ac6bc077251ff6e8ad3f1c194d04f
Giada Vecchi <giada.vecchi@studenti.unicam.it>
2026-06-21 12:15:44 +0200

    aggiunto medoti

--- src/main/java/it/unicam/cs/mpgc/rpg126362/controller/IGameController.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126362/controller/IGameController.java
@@ -1,5 +1,23 @@
 package it.unicam.cs.mpgc.rpg126362.controller;
 
-public class IGameController {
-    
+import java.io.IOException;
+
+import it.unicam.cs.mpgc.rpg126362.model.GameState;
+
+/**
+* Contract for the game's main controller.
+* Responsibilities: Define game flow operations (start, load, progression).
+*/
+public interface IGameController {
+
+    void startNewGame(String trainerName, String starterName);
+
+    void loadGame() throws IOException;
+
+    GameState getState();
+
+    IBattleController getBattleController();
+
+    GameSaveService getGameSaveService();
+
 }
